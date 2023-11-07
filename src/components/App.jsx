2485dc3fd157a9df16210f7003c3e950b08815cf
SorinaHMinea <sorina.hodorog@yahoo.com>
2023-11-07 15:24:12 +0200

    GATA profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import Card from './Card/Card';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <main>
+      <Card />
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: 'red',
+        }}
+      >
+        my react app
+      </div>
+    </main>
   );
 };
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,45 @@
+import user from './user.json';
+import styles from './Card.module.css';
+import PropTypes from 'prop-types';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="User avatar" className={styles.avatar} />
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        {Object.keys(stats).map((stat, index) => (
+          <li key={index}>
+            <span className={styles.label}>{stat}</span>
+            <span className={styles.quantity}>{stats[stat]}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const UserProfile = () => (
+  <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
+);
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object,
+};
+
+export default UserProfile;
